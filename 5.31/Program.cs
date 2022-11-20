2f0c01d2125022f0273000fd04cacb6137178960
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:04:48 +0300

    Add files via upload

---
+++ 5.31/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5._31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 2 * a;
+            int c = 0;
+            for (int i = a; a <= b; a++)
+                c += a * a;
+            Console.WriteLine(c);
+        }
+    }
+}
